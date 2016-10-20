c9073424971ebec2871d2535e473e33893ca5ade
dan-nl <d_entous@yahoo.com>
2016-10-20 17:54:37 +0200

    changed return value and added jsdoc info

--- app/helpers/api/get-project-pin-ids.js
+++ app/helpers/api/get-project-pin-ids.js
@@ -7,15 +7,21 @@ var getApiPromise = require( 'node-historypin' ).getApiPromise;
 var getPinRequestOptions = require( './get-project-pin-request-options' );
 var getPinsFromJSON = require( './get-project-pins-from-json' );
 
+/**
+ * @param {batch_job} batch_job
+ * @throws {Error}
+ * @returns {Promise.<[{}]>}
+ */
 function getProjectPinIds( batch_job ) {
   return getApiPromise( getPinRequestOptions( batch_job ) )
     .then(
       /**
        * @param {Object} api_result
+       * @returns {[{}]}
        */
       function ( api_result ) {
         if ( !api_result ) {
-          return '';
+          return [ {} ];
         }
 
         return getPinsFromJSON( JSON.parse( api_result.body ) );
